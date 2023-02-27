fa1c45fcf29f6926beec7fa38fa16f289f15feb1
nitesh004 <niteshkrjain2004@gmail.com>
2023-02-27 17:58:36 +0530

    comiit

---
+++ String.java
@@ -0,0 +1,9 @@
+class String{
+    public static void main(String[] args) {
+        for(String s : args)
+        {
+            System.out.println(s);
+        }
+        System.out.println("Hello ");
+    }
+}
